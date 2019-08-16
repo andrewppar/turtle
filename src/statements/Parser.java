1c98b605a517c0ff97cc517641246fa4aabc3fc8
Andrew Parisi <andrew.p.parisi@gmail.com>
2019-08-15 22:09:40 -0400

    This patch adds updated support for the parser. It still isn't completely bug free, but it now accounts for uri endings and startings and can handle some of the abbreviated syntax when they are not combined.

--- src/statements/Parser.java
+++ src/statements/Parser.java
@@ -7,30 +7,57 @@ public class Parser {
   private String current_subject ; 
   private String current_predicate ; 
   private String current_object ; 
-
-  //Start HERE : This needs a new field that tracks whether we're inside a uri or not. If we are then we should ignore ';', '.', and ','. So look for '<' and '>'
+  private Boolean within_uri ; 
 
   public void parse (String contents, StatementIndex statement_index) { 
-    CharacterIterator iterator = new StringCharacterIterator (contents)  ; 
+    CharacterIterator iterator = new StringCharacterIterator (contents)  ; this.current_uri       = ""; 
+    this.current_subject   = ""; 
+    this.current_predicate = ""; 
+    this.current_object    = ""; 
+    this.within_uri =false  ;
     while ( iterator.current() != CharacterIterator.DONE ) { 
       Character character = iterator.current() ; 
-      if (character == ' ') { 
-        if (this.last_char != character )  { 
+//      System.out.println("SPIN");
+//      System.out.println("character") ; 
+//      System.out.println(character) ; 
+//      System.out.println("within_uri")      ; 
+//      System.out.println(this.within_uri)      ; 
+//      System.out.println("current_uri"); 
+//      System.out.println(this.current_uri); 
+//      System.out.println("current_subject")  ; 
+//      System.out.println(this.current_subject)  ; 
+//      System.out.println("current_predicate"); 
+//      System.out.println(this.current_predicate); 
+//      System.out.println("current_object")  ; 
+//      System.out.println(this.current_object)  ; 
+      if ( this.within_uri ) { 
+        this.current_uri = this.current_uri + character ; 
+        if (character == '>' ) {
           this.update_parser_with_uri(character,statement_index) ; 
+          this.within_uri=false  ;
+  //        System.out.println("IN") ; 
+  //        System.out.println(current_uri) ; 
+           
+        } 
+      }
+      else { 
+        if (character == ';' ) { 
+          this.end_statement(character, statement_index) ; 
         }
-      }
-      else if (character == ';' ) { 
-        this.end_statement(character, statement_index) ; 
-      }
-      else if (character == ',') { 
-        this.end_statement(character, statement_index) ; 
+        else if (character == ',') { 
+          this.end_statement(character, statement_index) ; 
+        } 
+        else if (character == '.') { 
+          this.end_statement(character, statement_index) ; 
+        }
+        else if (character == '<') { 
+          this.current_uri = this.current_uri + character ; 
+          this.within_uri = true ; 
+        }
+        //          else {
+        //            this.current_uri=this.current_uri + character ; 
+        //          }
       } 
-      else if (character == '.') { 
-        this.end_statement(character, statement_index) ; 
-      }
-      else {
-        this.current_uri=this.current_uri + character ; 
-      }
       iterator.next () ; 
     }
   }
@@ -38,20 +65,28 @@ public class Parser {
   private void update_parser_with_uri (Character character, StatementIndex statement_index)  { 
     if ( this.current_subject == "" ) { 
       //if there's no subject set the subject to current_uri
+    //  System.out.println("Adding Subject"); 
       this.current_subject  = this.current_uri  ; 
+      this.current_uri = ""  ; 
     }
     else if (this.current_predicate == "" ) {
       // if there is  a subject but no predicate, set it to current_uri
+      //System.out.println("Adding Predicate"); 
       this.current_predicate = this.current_uri ; 
+      this.current_uri = ""  ; 
     }
     else if (this.current_object == "" ) { 
       // if there is a subject and predicate but no object set it to current_uri
+      //System.out.println("Adding Object"); 
       this.current_object  = this.current_uri  ; 
+      this.current_uri = ""  ; 
     }
     else { 
       // Index the statement and reset the parser
       Statement statement = new Statement () ; 
       statement.addArgs(this.current_subject, this.current_predicate, this.current_object) ; 
+      //System.out.println("Adding Statemetn in UPDATE"); 
+      //System.out.println(statement); 
       statement_index.addStatement(statement) ; 
       this.reset(character) ; 
     }
