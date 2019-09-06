60c6af1b91866de26ff06f82c741cc7a8dbcfcaa
Andrew Parisi <andrew.p.parisi@gmail.com>
2019-09-06 08:12:51 -0400

    This commit fixes a bug with forward inference. Prior to  this commit, we would never perform forward inference because in the recursive case, we require that the statement for which we are performig forward inference does not exist. But the caller of that function would always call the recursive function with a subject and target from a statement that already existed. We've updated the caller of the recursive function to gather the immediate super classes of the class that first target class to perform the recursive forward inference on.

--- src/statements/ForwardInferenceEngine.java
+++ src/statements/ForwardInferenceEngine.java
@@ -16,38 +16,45 @@ public class ForwardInferenceEngine {
       HashMap<String,LinkedList<String>> subject_object_dictionary = predicate_index.get("<http://www.w3.org/2002/07/owl#subClassOf>") ; 
 
       LinkedList<String> target_classes_surveyed = new LinkedList<String> () ; 
-      System.out.println(type_statements) ;
-      System.out.println(type_statements.size()) ; 
+//      System.out.println(type_statements) ;
+//      System.out.println(type_statements.size()) ; 
       int size = type_statements.size() ;
 
       //Get the  graph for those statements 
       for (int i = 0; i < size  ; i++) { 
-        System.out.println("Base Class " + i ) ;
+        //System.out.println("Base Class " + i ) ;
         Statement statement = type_statements.get(i);
         String subject = statement.subject ; 
-        String target_class = statement.object ; 
-        target_classes_surveyed = add_subclass_expressions_to_quiescence(subject, target_class, statement_index, target_classes_surveyed) ; 
+        if (subject_object_dictionary.containsKey(statement.object)) {
+          LinkedList<String>  target_classes = subject_object_dictionary.get(statement.object) ;
+          int target_classes_size = target_classes.size() ; 
+          for (int target_index = 0 ; target_index < target_classes_size ; target_index++) { 
+            String target_class = target_classes.get(target_index) ; 
+            target_classes_surveyed = add_subclass_expressions_to_quiescence(subject, target_class, statement_index, target_classes_surveyed) ; 
+          }
+        }
       }
     } 
   }
 
   private static LinkedList<String> add_subclass_expressions_to_quiescence(String subject, String target_class, StatementIndex statement_index, LinkedList<String> classes_surveyed) { 
-    System.out.println("Adding Recursive For") ; 
-    System.out.println(subject) ; 
-    System.out.println(target_class) ; 
+    //System.out.println("Adding Recursive For") ; 
+    //System.out.println(subject) ; 
+    //System.out.println(target_class) ; 
     classes_surveyed.add(target_class) ; 
     //Depth first Adding of statements
     HashMap<String,HashMap<String,LinkedList<String>>> predicate_index  = statement_index.predicateObjectIndex ; 
     HashMap<String,LinkedList<String>> subject_object_dictionary = predicate_index.get("<http://www.w3.org/2002/07/owl#subClassOf>") ; 
     Statement new_statement = new Statement () ; 
     new_statement.addArgs(subject, "a", target_class) ;
-    System.out.println(new_statement) ; 
+    //System.out.println(new_statement) ; 
     Boolean statement_added = statement_index.addStatement(new_statement) ; 
     //@todo handle looping taxonomies 
-    System.out.println(subject_object_dictionary.containsKey(target_class)) ; 
+    //System.out.println(subject_object_dictionary.containsKey(target_class)) ; 
+    //System.out.println(statement_added) ; 
 
     if ( statement_added  && subject_object_dictionary.containsKey(target_class)) {
-      System.out.println(subject_object_dictionary) ;
+      //System.out.println(subject_object_dictionary) ;
       LinkedList<String> new_target_classes = subject_object_dictionary.get(target_class) ; 
       for (int i = 0 ; i < new_target_classes.size() ; i++) {
         String new_target_class  = new_target_classes.get(i) ; 
@@ -58,4 +65,5 @@ public class ForwardInferenceEngine {
 
     return classes_surveyed ;
   } 
+
 }
